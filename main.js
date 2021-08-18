2dbe2ddcc30c9a58e3c88a52ad232af290e6cb9b
Armin Sayar <armin@m.lo.agency>
2021-08-18 22:19:42 +0430

    feat
    New color and popup page added!
    - dark mode added
    - new popup page for voice icon added (without transition)
    - responsive added

---
+++ main.js
@@ -0,0 +1,18 @@
+'use strict'
+
+$("#voiceD").click(function(){
+    $("section").css("display", "none");
+    $("footer").css("display", "none");
+    $("#textD").css("display", "none");
+    $("body").addClass("vp-a");
+    $('button[name="xb"]').removeClass("vpb-d");
+    $('div[name="vpd-main"]').removeClass("vpd-d");
+});
+$('button[name="xb"]').click(function(){
+    $("section").css("display", "block");
+    $("footer").css("display", "block");
+    $("#textD").css("display", "block");
+    $("body").removeClass("vp-a");
+    $('button[name="xb"]').addClass("vpb-d");
+    $('div[name="vpd-main"]').addClass("vpd-d");
+})
