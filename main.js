fdc8d5f732ab8de82aec6d877912eadfbc38bf9f
Armin Sayar <armin@m.lo.agency>
2021-08-19 18:10:48 +0430

    feat
    Voice app page completed.
    - transition added
    - new active effect for voice icon

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
 'use strict'
 
-$("#voiceD").click(function () {
+$("#voiceD").click(function menuOn() {
     $("section").css("display", "none");
     $("footer").css("display", "none");
     $("#textD").css("display", "none");
     $("body").addClass("vp-a");
     $('button[name="xb"]').removeClass("vpb-d");
+    $('div[name="vptext"]').removeClass("vpt-d");
+    $('div[name="vpmicro"]').removeClass("m-d");
     $('div[name="vpd-main"]').removeClass("vpd-d");
 });
-$('button[name="xb"]').click(function () {
+
+$('button[name="xb"]').click(function menuOff() {
     $("section").css("display", "block");
     $("footer").css("display", "block");
     $("#textD").css("display", "block");
     $("body").removeClass("vp-a");
     $('button[name="xb"]').addClass("vpb-d");
+    $('div[name="vptext"]').addClass("vpt-d");
+    $('div[name="vpmicro"]').addClass("m-d");
     $('div[name="vpd-main"]').addClass("vpd-d");
-})
+});
+
+$('div[name="vpd-main"]').click(function () {
+    if ($("body").hasClass("vp-a")) {
+        $("section").css("display", "block");
+        $("footer").css("display", "block");
+        $("#textD").css("display", "block");
+        $("body").removeClass("vp-a");
+        $('button[name="xb"]').addClass("vpb-d");
+        $('div[name="vptext"]').addClass("vpt-d");
+        $('div[name="vpmicro"]').addClass("m-d");
+        $('div[name="vpd-main"]').addClass("vpd-d");
+    }
+});
