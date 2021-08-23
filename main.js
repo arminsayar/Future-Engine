168ed1f597c3e3f1371ef62a33690ff97d28cc50
Armin Sayar <armin@m.lo.agency>
2021-08-23 16:45:57 +0430

    fix
    fix some bugs for server

--- main.js
+++ main.js
@@ -55,33 +55,34 @@ $('#footerT7, .foot-li').click(function (event) {
 // ----drop-down-menu----
 
 let appID = document.querySelector("#appID");
-let DDmenu =document.querySelector(".drop-down-menu");
-let iconrow =document.querySelector(".row");
+let DDmenu = document.querySelector(".drop-down-menu");
+let iconrow = document.querySelector(".row");
 let applist = document.querySelector(".applist");
 
-$("#appID").click(function(){
+$("#appID").click(function () {
     $(".drop-down-menu").css("display", "block");
 });
 
-$("html").click(function(){
+$("html").click(function () {
     $(".drop-down-menu").css("display", "none");
 });
 
-$("#appID, .icon, .drop-down-menu, .icon1, .more-button, .more").click(function(e){
+$("#appID, .icon, .drop-down-menu, .icon1, .more-button, .more").click(function (e) {
     e.stopPropagation();
 });
 
 //----Arts and Culture----
- let iconarts =document.querySelector(".arts");
- let longcaption=document.querySelector(".long");
- let hovercaption=document.querySelector(".hovercaption");
+let iconarts = document.querySelector(".arts");
+let longcaption = document.querySelector(".long");
+let hovercaption = document.querySelector(".hovercaption");
 
- iconarts.addEventListener("mouseover" , showtext);
- iconarts.addEventListener("mouseleave" , hidetext);
- function showtext (){
-      longcaption.style.whiteSpace = "unset";}
-     //hovercaption.style.display="block";
+iconarts.addEventListener("mouseover", showtext);
+iconarts.addEventListener("mouseleave", hidetext);
+function showtext() {
+    longcaption.style.whiteSpace = "unset";
+}
+//hovercaption.style.display="block";
 
- function hidetext(){
-       longcaption.style.whiteSpace = "nowrap"; 
- }
+function hidetext() {
+    longcaption.style.whiteSpace = "nowrap";
+}
