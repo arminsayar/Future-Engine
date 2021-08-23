ce1cf472c35c01309b87652479cc67be95002e66
Armin Sayar <armin@m.lo.agency>
2021-08-23 19:14:57 +0430

    feat(web-server)
    server files changed
    - server.js updated
    - app.js added with expressjs
    - public and views folders added

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const path = require('path');
 
 
 http.createServer((req, res) => {
-    let ImageDir = path.join(__dirname, 'images', req.url);
     let method = req.method;
+    
     if (req.url === "/") {
-        fs.readFile("./index.html", "UTF-8", function (err, html) {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), "UTF-8", function (err, html) {
             res.writeHead(200, { "Content-Type": "text/html" });
             res.end(html);
         });
     } else if (req.url.match(".css")) {
-        var cssPath = path.join(__dirname, './', req.url);
+        var cssPath = path.join(__dirname, 'public', req.url);
         var fileStream = fs.createReadStream(cssPath, "UTF-8");
         res.writeHead(200, { "Content-Type": "text/css" });
         fileStream.pipe(res);
@@ -22,7 +22,7 @@ http.createServer((req, res) => {
         res.writeHead(200, { "Content-Type": "image/png" });
         fileStream.pipe(res);
     } else if (req.url.match(".js")) {
-        var jsPath = path.join(__dirname, './', req.url);
+        var jsPath = path.join(__dirname, 'public', req.url);
         var fileStream = fs.createReadStream(jsPath, "UTF-8");
         res.writeHead(200, { "Content-Type": "text/javascript" });
         fileStream.pipe(res);
